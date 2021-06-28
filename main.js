6b721681eeb1a88d0fdf5d59e4999016ea450bd6
tatsuya-sy <82197633+tatsuya-sy@users.noreply.github.com>
2021-06-28 22:50:14 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const headerModule = (() => {
+  const menuBtn = document.getElementById("menu-btn");
+  const mask = document.getElementById("mask");
+  const header = document.getElementById("header");
+
+  return {
+    clickedMenuBtn: menuBtn.addEventListener("click", () => {
+      menuBtn.classList.toggle("clicked");
+      mask.classList.toggle("clicked");
+      header.classList.toggle("clicked");
+    }),
+    clickedMask: mask.addEventListener("click", () => {
+      menuBtn.classList.remove("clicked");
+      mask.classList.remove("clicked");
+      header.classList.remove("clicked");
+    }),
+  };
+})();
+
+const footerModule = (() => {
+  const accordions = document.getElementsByClassName('accordion');
+  console.log(accordions[0]);
+
+  for (let i = 0; i < accordions.length; i++) {
+    accordions[i].addEventListener('click', () => {
+      accordions[i].classList.toggle('clicked');
+    })
+  }
+})();
+
